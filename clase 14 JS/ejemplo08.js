a48a93bb698d42bcf78f06059dc3d7f3bea45aaf
Brian Puyol <brianpuyol1997@gmail.com>
2024-05-28 19:20:26 -0300

    clase

---
+++ clase 14 JS/ejemplo08.js
@@ -0,0 +1,21 @@
+function validarForm() {
+    const nombreValido = true; //que alguien escriba algo
+    const passWordValido = validarLargo(document.form.text1.value, 5); //llama a la funcion de la linea 18
+
+    const resultado = nombreValido && passWordValido;
+    if (resultado) {
+        alert('ES VÁLIDO')
+    } else {
+        alert('NOOOOO ES VÁLIDO')
+    }
+
+}
+
+
+
+
+
+function validarLargo(texto, largoRequerido) { //texto = document.form.text1.value
+    return texto.length >= largoRequerido      //largoRequerido
+
+}
